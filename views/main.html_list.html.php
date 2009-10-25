73882477cb665684d0a125d86b1f0c97688f6c95
Lance Wicks <lw@judocoach.com>
2009-10-25 22:34:29 +0000

    Minor ammenedment to the HTML dojo list

--- views/main.html_list.html.php
+++ views/main.html_list.html.php
@@ -2,15 +2,15 @@
 <h1>DOJO LIST</h1>
 <hr>
 <hr>
-<ul>
-<?php foreach ($DojoList as $dojo) { 
 
+<?php foreach ($DojoList as $dojo) { 
+		
 		 foreach ($dojo as $key => $value)
 		{
 			print "<li>&nbsp; $key: $value</li>";
 		} 
-  ?><hr><?php
+	print '<p>&nbsp;</p>';
  } ?>
-</ul>
+
 
 
