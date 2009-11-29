a6b58f98f8771705ae8db5b56b520e5fc8e4b6c9
Lance Wicks <lw@judocoach.com>
2009-11-29 22:12:20 +0000

    Moved the license file

--- views/default_layout.php
+++ views/default_layout.php
@@ -125,7 +125,7 @@ Released   : 20090622
 					<p>DojoList is an open source project by Lance Wicks. The project aims to provide a system to make it easy for a association to maintain a list of Judo clubs.</p>
 					<p>The project is hosted at <a href="http://github.com/lancew/DojoList">http://github.com/lancew/DojoList</a> where you can access all the source code, record bugs, etc.</p>
 					<p>This project has been built on the <a href="http://sofa-design.net/limonade/" title="Limonade PHP micro-framework">Limonade PHP micro-framework</a> and the <a href="http://jquery.com/">jquery</a> and <a href="http://www.mapstraction.com/">mapstraction</a> javascript libraries.</p>
-					<p>The DojoList project is licensed under a <a href="<?php echo option('base_uri') ?>agpl-3.0.txt">AGPL license.<br /><img src="<?php echo option('images_dir') ?>/agplv3-88x31.png" alt="agplv3-88x31" width="88" height="31"/></a></p>	
+					<p>The DojoList project is licensed under a <a href="<?php echo option('data_dir') ?>/agpl-3.0.txt">AGPL license.<br /><img src="<?php echo option('images_dir') ?>/agplv3-88x31.png" alt="agplv3-88x31" width="88" height="31"/></a></p>	
 					<p><script type="text/javascript" src="http://www.ohloh.net/p/459913/widgets/project_thin_badge.js"></script></p>
 					
 				</li>
--- views/main.html_list.html.php
+++ views/main.html_list.html.php
@@ -6,23 +6,24 @@
 <?php foreach ($DojoList as $dojo) {
 
 	foreach ($dojo as $key => $value) {
-
+		// The following line skips fields if they are blank.
 		if (!$value){continue;}
 
+		// Display the email address as HTML link.
 		if ($key =='ContactEmail') {
 			print "<li>&nbsp; $key: <a href='mailto:$value'>$value</a></li>";
 			continue;
 
 		}
 
-
+		// Display the email address as HTML link.
 		if ($key =='ClubWebsite') {
 			print "<li>&nbsp; $key: <a href='http://$value'>$value</a></li>";
 			continue;
 		}
 
-		
-			print "<li>&nbsp; $key: $value</li>";
+		// Default: Display the key and Value for the fields
+		print "<li>&nbsp; $key: $value</li>";
 		
 
 	}
