58ac0acf99d247beb4db74e19809e24c81f99d2f
Lance Wicks <lw@judocoach.com>
2009-12-01 23:25:51 +0000

    Added some comments to main controller based on phpcs results

--- controllers/main.php
+++ controllers/main.php
@@ -1,19 +1,47 @@
 <?php
- 
-# GET /
+
+/**
+  * DojoList main controller file
+  *
+  * This controller is for the main pages for the DojoList Application.
+  *
+  * PHP version 5
+  *
+  * LICENSE: please see the AGPL license file is data/agpl-3.0.txt
+  *
+  * @category  MainController
+  * @package   DojoList
+  * @author    Lance WIcks <lw@judocoach.com>
+  * @copyright 2009 Lance Wicks
+  * @license   http://www.gnu.org/licenses/agpl.html  AGPL License 3.0
+  * @link      http://github.com/lancew/DojoList
+ */
+
+
 function main_page() {
-    return html('main.html.php');
+	return html('main.html.php');
 }
 
+
+/**
+ *
+ *
+ * @return unknown
+ */
 function html_list() {
 	if (file_exists('data/dojo.xml')) {
-	$xml = simplexml_load_file('data/dojo.xml');
+		$xml = simplexml_load_file('data/dojo.xml');
+		//print_r($xml);
 	} else {
 		exit('Failed to open dojo.xml.');
 	}
 
 	set('DojoList', $xml);
 
-	
-    return html('main.html_list.html.php');
+
+	return html('main.html_list.html.php');
 }
+
+
+
+?>
