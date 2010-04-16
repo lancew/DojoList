95b046e7b80380a4d194e0a3be50867d5a7af1fd
Lance Wicks <lw@judocoach.com>
2010-04-16 16:26:17 +0100

    added attribution to Ulrich Wisser to default layout

--- tests/dojo_test.php
+++ tests/dojo_test.php
@@ -2,7 +2,7 @@
 require_once('simpletest/unit_tester.php');
 require_once('simpletest/reporter.php');
 require_once('lib/dojo.model.php');
-require_once('lib/dojo.model.php');
+
 
 class TestOfDojolist extends UnitTestCase {
 
@@ -26,14 +26,20 @@ class TestOfDojolist extends UnitTestCase {
 	}		
 	
 	
-	//function testCreate_dojo() {
-	   // The Create_dojo function and test needs modifying to stop using $_FILES
-	//	$this->assertEqual(Create_dojo(), 'Dojo Created');
-	//}
+	function testCreate_dojo() {
+        if (Find_dojo('test_dojo')->DojoName != 'test_dojo') 
+        {
+        
+        $dojo_array = array('DojoName' => 'test_dojo', 'ClubWebsite' => 'url', 'Latitude' => '0', 'Longitude' => '0' );
+	    $this->assertEqual(Create_dojo($dojo_array), 'Dojo Created');
+	    } else {
+	       $this->fail('test_dojo found prior to create test');
+	    }
+	}
 	
 	function testDelete_dojo() {
 		Delete_dojo('test_dojo');
-		$this->assertFalse(Find_dojo('test_dojo'));
+		$this->assertFalse(Find_dojo('test_dojo'), 'test_dojo was not deleted');
 	}
     
 	
@@ -46,7 +52,7 @@ class TestOfDojolist extends UnitTestCase {
 		$pattern = '#</Longitude></Dojo></xml>#';
 		$result = preg_match($pattern, $xmlText);
 		//echo "result:".$result;
-		$this->assertTrue($result);
+		$this->assertTrue($result, 'The test for <Dojo/> tags at the end of the file failed');
 	}
 	
 }
