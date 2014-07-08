80564245d3e526cd512346b404feb40e8a057601
Lance Wicks <lw@judocoach.com>
2014-07-08 23:31:14 +0100

    Adding initial Judoka model code

---
+++ lib/judoka.model.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * DojoList Judoka Model file
+ *
+ * This file is the model for Judoka in the DojoList Application.
+ *
+ * PHP version 5
+ *
+ * LICENSE: please see the AGPL license file is data/agpl-3.0.txt
+ *
+ * @category  JudokaModel
+ * @package   DojoList
+ * @author    Lance Wicks <lw@judocoach.com>
+ * @copyright 2013 Lance Wicks
+ * @license   http://www.gnu.org/licenses/agpl.html  AGPL License 3.0
+ * @link      http://github.com/lancew/DojoList
+ */
+
+require_once 'lib/data.model.php';
+require_once 'lib/rss.php';
+
+
+function Judoka_Create()
+{
+
+
+}
+
+function Judoka_Edit()
+{
+
+}
+
+function Judoka_Delete()
+{
+
+}
+
+function Judoka_View()
+{
+	
+}
+
+
+
+
+?>
